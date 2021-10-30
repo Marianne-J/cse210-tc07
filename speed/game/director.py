7c559508a553daa2016b9b65b7e086ef906b8aab
Noctumbras <stephen64s@byui.edu>
2021-10-30 14:26:29 -0500

    director implementation

--- speed/game/director.py
+++ speed/game/director.py
@@ -1,8 +1,8 @@
 from time import sleep
 from game import constants
-from game.food import Food
+from game.words import Words
 from game.score import Score
-from game.snake import Snake
+from game.buffer import Buffer
 
 class Director:
     """A code template for a person who directs the game. The responsibility of 
@@ -26,12 +26,12 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        self._food = Food()
         self._input_service = input_service
         self._keep_playing = True
         self._output_service = output_service
         self._score = Score()
-        self._snake = Snake()
+        self._buffer = Buffer()
+        self._words = Words()
         
     def start_game(self):
         """Starts the game loop to control the sequence of play.
@@ -52,8 +52,12 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        direction = self._input_service.get_direction()
-        self._snake.move_head(direction)
+        letter = self._input_service.get_letter()
+
+        if letter == "*":
+            self._buffer.clear_buffer()
+        else:
+            self._buffer.add_letter(letter)
 
     def _do_updates(self):
         """Updates the important game information for each round of play. In 
@@ -62,8 +66,12 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        self._handle_body_collision()
-        self._handle_food_collision()
+        buffer_contents = self._buffer.get_buffer()
+        match_count = self._words.check_words(buffer_contents)
+        self._score.add_points(3 * match_count)
+
+        self._words.move_words()
+
         
     def _do_outputs(self):
         """Outputs the important game information for each round of play. In 
@@ -74,36 +82,8 @@ class Director:
             self (Director): An instance of Director.
         """
         self._output_service.clear_screen()
-        self._output_service.draw_actor(self._food)
-        self._output_service.draw_actors(self._snake.get_all())
+        self._output_service.draw_actor(self._buffer)
+        self._output_service.draw_actors(self._words.get_words())
         self._output_service.draw_actor(self._score)
         self._output_service.flush_buffer()
-
-    def _handle_body_collision(self):
-        """Handles collisions between the snake's head and body. Stops the game 
-        if there is one.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        head = self._snake.get_head()
-        body = self._snake.get_body()
-        for segment in body:
-            if head.get_position().equals(segment.get_position()):
-                self._keep_playing = False
-                break
-
-    def _handle_food_collision(self):
-        """Handles collisions between the snake's head and the food. Grows the 
-        snake, updates the score and moves the food if there is one.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        head = self._snake.get_head()
-        if head.get_position().equals(self._food.get_position()):
-            points = self._food.get_points()
-            for n in range(points):
-                self._snake.grow_tail()
-            self._score.add_points(points)
-            self._food.reset() 
+        
